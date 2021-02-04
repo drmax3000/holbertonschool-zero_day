147cdf4ecdb9149bf91da8183f831b200bc3581b
drmax300 <drmax300@hotmail.com>
2021-02-04 03:12:55 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-**
- *main - Entry Point
+/**
+ *main - Entry point
  *
- * Return: Always 0 (Success)
+ *Return: Always 0 (Sucess)
  */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
